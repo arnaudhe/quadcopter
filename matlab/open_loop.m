bc819fa277b0a351b6bf7305a3a012e073a10eb0
arnaudhe <arnaud.henric@gmail.com>
2017-02-04 09:52:35 +0100

    Improve visualize 2D

--- matlab/open_loop.m
+++ matlab/open_loop.m
@@ -4,7 +4,7 @@ close all
 load('quad_constants.mat')
 
 speed = sqrt(quad_constants.mass * quad_constants.g/(4*quad_constants.ct));
-motors_speed = [speed + 10; speed + 10; speed; speed];
+motors_speed = [speed + 10; speed - 10; speed + 10; speed - 10];
 dt = 0.01; 
 
 lin_pos = [0; 0; 10];
@@ -20,7 +20,7 @@ result.ang_pos = ang_pos;
 result.ang_vel = ang_vel;
 result.input   = motors_speed;
 
-for t = dt:dt:20-dt
+for t = dt:dt:10
     [ang_pos, ang_vel, lin_pos, lin_vel] = plant_step_2(quad_constants, ang_pos, ang_vel, lin_pos, lin_vel, motors_speed, dt);
     result.t       = [result.t, t];
     result.dt      = [result.dt, dt];
--- matlab/visualize_2D.m
+++ matlab/visualize_2D.m
@@ -2,21 +2,66 @@ function visualize(result)
 
 figure
 
-subplot(2,2,1)
-plot(result.t, result.ang_pos)
-title('Angular position')
+subplot(4,3,1)
+plot(result.t, result.ang_vel(1,:), 'b')
+title('P')
+grid on
 
-subplot(2,2,2)
-plot(result.t, result.ang_vel)
-title('Angular velocity')
+subplot(4,3,2)
+plot(result.t, result.ang_vel(2,:), 'r')
+title('Q')
+grid on
 
-subplot(2,2,3)
-plot(result.t, result.lin_pos)
-title('Linear position')
+subplot(4,3,3)
+plot(result.t, result.ang_vel(3,:), 'g')
+title('R')
+grid on
 
-subplot(2,2,4)
-plot(result.t, result.lin_vel)
-title('Linear velocity')
+subplot(4,3,4)
+plot(result.t, result.ang_pos(1,:), 'b')
+title('Phi')
+grid on
+
+subplot(4,3,5)
+plot(result.t, result.ang_pos(2,:), 'r')
+title('Theta')
+grid on
+
+subplot(4,3,6)
+plot(result.t, result.ang_pos(3,:), 'g')
+title('Psi')
+grid on
+
+subplot(4,3,7)
+plot(result.t, result.lin_vel(1,:), 'b')
+title('U')
+grid on
+
+subplot(4,3,8)
+plot(result.t, result.lin_vel(2,:), 'r')
+title('V')
+grid on
+
+subplot(4,3,9)
+plot(result.t, result.lin_vel(3,:), 'g')
+title('W')
+grid on
+
+subplot(4,3,10)
+plot(result.t, result.lin_pos(1,:), 'b')
+title('X')
+grid on
+
+subplot(4,3,11)
+plot(result.t, result.lin_pos(2,:), 'r')
+title('Y')
+grid on
+
+subplot(4,3,12)
+plot(result.t, result.lin_pos(3,:), 'g')
+title('Z')
+grid on
+
 
 end
 

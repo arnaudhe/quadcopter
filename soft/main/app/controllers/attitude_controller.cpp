52068864018b8386bd98985976a0f5d082dd51aa
Arnaud Henric <arnaud.henric@gmail.com>
2019-04-22 11:35:27 +0200

    [soft] Update attitude controller global scheduling

--- soft/main/app/controllers/attitude_controller.cpp
+++ soft/main/app/controllers/attitude_controller.cpp
@@ -1,18 +1,26 @@
 #include <app/controllers/attitude_controller.h>
 #include <app/controllers/attitude_controller_conf.h>
+#include <esp_log.h>
 
 AttitudeController::AttitudeController(float period)
 {
     _period = period;
+    _barometer_timing = 0.0;
+    _barometer_waiting = false;
 
     _euler_observer = new EulerObserver(period);
     _height_observer = new HeightObserver(period);
 
-    _i2c    = new I2cMaster(I2C_MASTER_NUM);
-    _marg   = new Marg(_i2c);
+    _i2c        = new I2cMaster(I2C_MASTER_NUM);
+    _marg       = new Marg(_i2c);
+    _baro       = new Barometer(_i2c);
+    _ultrasound = new UltrasoundSensor();
 
     _i2c->init();
     _marg->init();
+    _baro->init();
+
+    _baro->read(_barometer);
 
     _height_controller = new Controller(period, 
                                         new Pid(period, ATTITUDE_PID_HEIGHT_KP, ATTITUDE_PID_HEIGHT_KP, ATTITUDE_PID_HEIGHT_KP),
@@ -34,6 +42,8 @@ AttitudeController::AttitudeController(float period)
                        new Motor(ATTITUDE_MOTOR_FRONT_RIGHT_PULSE_CHANNEL, ATTITUDE_MOTOR_FRONT_RIGHT_PULSE_PIN),
                        new Motor(ATTITUDE_MOTOR_REAR_LEFT_PULSE_CHANNEL,   ATTITUDE_MOTOR_REAR_LEFT_PULSE_PIN),
                        new Motor(ATTITUDE_MOTOR_REAR_RIGHT_PULSE_CHANNEL,  ATTITUDE_MOTOR_REAR_RIGHT_PULSE_PIN));
+
+    ESP_LOGI("attitude_controller", "Init done");
 }
 
 void AttitudeController::update(void)
@@ -42,7 +52,8 @@ void AttitudeController::update(void)
     float  ax, ay, az;           /* accelero in drone frame (sensor data) */
     float  ax_r, ay_r, az_r;     /* accelero in earth frame */
     float  mx, my, mz;           /* magneto in drone frame (sensor data) */
-    double baro;                 /* barometer sensor data */
+    double temp;                 /* barometer temperature sensor data */
+    double ultrasound = 0.0;
     float  roll, pitch, yaw;     /* previous values */
 
     /* Read the sensors */
@@ -50,7 +61,22 @@ void AttitudeController::update(void)
     _marg->read_acc(&ax, &ay, &az);
     _marg->read_gyro(&gx, &gy, &gz);
     _marg->read_mag(&mx, &my, &mz);
-    _baro->read(baro);
+
+    if (_barometer_timing >= (0.031))
+    {
+        _baro->read(_barometer);
+        _barometer_timing = 0.0;
+        _barometer_waiting = false;
+    }
+    else if ((_barometer_timing >= 0.005) && (_barometer_waiting == false))
+    {
+        _barometer_waiting = true;
+        _baro->read_temperature(temp);
+        // printf("temperature %f\n", temp);
+    }
+    _barometer_timing += _period;
+
+    ultrasound = _ultrasound->height();
 
     /* Estimate the attitude */
 
@@ -60,12 +86,18 @@ void AttitudeController::update(void)
 
     _euler_observer->update(gx, gy, gz, ax, ay, az, mx, my, mz);
     _euler_observer->rotate(ax, ay, az, &ax_r, &ay_r, &az_r);
-    _height_observer->update(az_r, baro, 0.0);
+    _height_observer->update(-az_r + 1.0f, _barometer, ultrasound);
 
     _roll_speed   = (_euler_observer->roll()  - roll)  / _period;
     _pitch_speed  = (_euler_observer->pitch() - pitch) / _period;
     _yaw_speed    = (_euler_observer->yaw()   - yaw)   / _period;
     _height_speed = _height_observer->vertical_speed();
+
+    // printf("%f\n", ultrasound);
+    // printf("%f\n", _barometer);
+    printf("%f\n", _height_observer->height());
+    // printf("%f;%f;%f\n", 180.0 * _euler_observer->roll() / 3.1416, 180.0 * _euler_observer->pitch() / 3.1416, 180.0 * _euler_observer->yaw() / 3.1416);
+    // printf("%f;%f;%f\n", ax_r, ay_r, az_r);
 
     /* Compute the controls */
 
@@ -76,7 +108,7 @@ void AttitudeController::update(void)
 
     /* Apply the controls to the motors */
 
-    _mixer->update(_height_controller->command(), _roll_controller->command(), _pitch_controller->command(), _yaw_controller->command());
+    // _mixer->update(_height_controller->command(), _roll_controller->command(), _pitch_controller->command(), _yaw_controller->command());
 }
 
 void AttitudeController::set_height_target(Controller::Mode mode, float target)
--- soft/main/app/controllers/attitude_controller.h
+++ soft/main/app/controllers/attitude_controller.h
@@ -5,8 +5,10 @@
 
 #include <hal/marg.h>
 #include <hal/barometer.h>
+#include <hal/ultrasound.h>
 
 #include <periph/i2c_master.h>
+#include <periph/timer.h>
 
 #include <app/observers/euler_observer.h>
 #include <app/observers/height_observer.h>
@@ -16,17 +18,22 @@ class AttitudeController
 
 private:
 
-    I2cMaster      * _i2c;
-    Marg           * _marg;
-    Barometer      * _baro;
-    HeightObserver * _height_observer;
-    EulerObserver  * _euler_observer;
-    Controller     * _height_controller;
-    Controller     * _roll_controller;
-    Controller     * _pitch_controller;
-    Controller     * _yaw_controller;
-    Mixer          * _mixer;
-    float            _period;
+    Timer            * _timer;
+    I2cMaster        * _i2c;
+    Marg             * _marg;
+    Barometer        * _baro;
+    UltrasoundSensor * _ultrasound;
+    HeightObserver   * _height_observer;
+    EulerObserver    * _euler_observer;
+    Controller       * _height_controller;
+    Controller       * _roll_controller;
+    Controller       * _pitch_controller;
+    Controller       * _yaw_controller;
+    Mixer            * _mixer;
+    float              _period;
+    double             _barometer;
+    float              _barometer_timing;
+    bool               _barometer_waiting;
 
     float            _roll_speed;
     float            _pitch_speed;

dfd42751fc5a2c8f2898eb1d22a033dccc190021
arnaudhe <arnaud.henric@gmail.com>
2019-04-28 23:46:37 +0200

    [soft] Add registry test program

---
+++ soft/main/tests/registry/hal/udp_server.cpp
@@ -0,0 +1,137 @@
+#include <hal/udp_server.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <esp_log.h>
+
+class UdpServerError : public runtime_error
+{
+public:
+    UdpServerError(const char *w) : runtime_error(w) {}
+};
+
+UdpServer::UdpServer(string name, int port)
+    : _name(name)
+    , _port(port)
+{
+    struct sockaddr_in destAddr;
+    destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    destAddr.sin_family      = AF_INET;
+    destAddr.sin_port        = htons(_port);
+
+    _socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(_socket == -1)
+    {
+        ESP_LOGE(_name.c_str(), "Could not create UDP socket");
+        return;
+    }
+
+    int err = ::bind(_socket, (struct sockaddr *)&destAddr, sizeof(destAddr));
+    if(err != 0)
+    {
+        close(_socket);
+        ESP_LOGE(_name.c_str(), "Could not bind UDP socket");
+    }
+
+    ESP_LOGI(_name.c_str(), "Server ready");
+}
+
+UdpServer::~UdpServer()
+{
+    close(_socket);
+}
+
+void UdpServer::register_callback(function<void(string, string&)> callback)
+{
+    _callback = callback;
+}
+
+bool UdpServer::recv(string &msg)
+{
+    char buffer[1024];
+    int  len;
+
+    len = ::recv(_socket, buffer, sizeof(buffer) - 1, 0);
+    if (len > 0)
+    {
+        buffer[len] = '\0';
+        string str(buffer);
+        msg = str;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool UdpServer::recvfrom(string &msg, string &address, int &port)
+{
+    struct sockaddr_in source_addr;
+    socklen_t          source_addr_len = sizeof(source_addr);
+    char               buffer[1024];
+    char             * addr_char;
+    int                len;
+
+    len = ::recvfrom(_socket, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&source_addr, &source_addr_len);
+
+    if (len > 0)
+    {
+        buffer[len] = '\0';
+        string str(buffer);
+        msg = str;
+        addr_char = inet_ntoa(source_addr.sin_addr);
+        string addr_str(addr_char);
+        address = addr_str;
+        port = ntohs(source_addr.sin_port);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool UdpServer::sendto(string msg, string address, int port)
+{
+    struct sockaddr_in dest_addr;
+    int                len;
+
+    inet_aton(address.c_str(), &dest_addr.sin_addr);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port   = htons(port);
+
+    len = ::sendto(_socket, msg.c_str(), strlen(msg.c_str()), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+
+    if (len > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void UdpServer::start(void)
+{
+    string request;
+    string response;
+    string ip_src;
+    int    port_src;
+
+    while(1)
+    {
+        if (recvfrom(request, ip_src, port_src))
+        {
+            ESP_LOGI(_name.c_str(), "received from %s:%d : %s", ip_src.c_str(), port_src, request.c_str());
+            _callback(request, response);
+            ESP_LOGI(_name.c_str(), "send to %s:%d : %s", ip_src.c_str(), port_src, response.c_str());
+            sendto(response, ip_src, port_src);
+        }
+    }
+}
---
+++ soft/main/tests/registry/test_registry.cpp
@@ -0,0 +1,14 @@
+#include <hal/udp_server.h>
+#include <data_model/data_ressources_registry.h>
+#include <data_model/json_protocol.h>
+
+int main(int argc, char ** argv)
+{
+    UdpServer * server = new UdpServer("control_server", 5000);
+    DataRessourcesRegistry * registry = new DataRessourcesRegistry("../../../../../data_model/data_model.json");
+    JsonDataProtocol * protocol = new JsonDataProtocol(server, registry);
+
+    server->start();
+
+    return 0;
+}

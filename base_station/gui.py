1372bed5e9fa71ae304c147215cc245c455157b2
arnaudhe <arnaud.henric@gmail.com>
2019-04-28 23:47:23 +0200

    [base station] Add UDP server response handling

--- base_station/gui.py
+++ base_station/gui.py
@@ -1,8 +1,11 @@
 import json
 import sys
+import socket
+import time
 from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QMainWindow, QTextEdit, QVBoxLayout, QHBoxLayout, QSlider, QLabel, QPushButton, QCheckBox, QComboBox, QGroupBox, QScrollArea
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt, pyqtSignal, QRect
+from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf, DNSAddress
 
 class FloatSlider(QSlider):
 
@@ -56,9 +59,16 @@ class DataRessourceWidget(QWidget):
         self.layout.addWidget(value_widget)
         self.layout.addWidget(self.button)
         self.setLayout(self.layout)
+        self.read_pending = False
 
     def on_button_clicked(self):
-        self.read_callback(self.key)
+        status, value = self.read_callback(self.key)
+        if status == True:
+            self.read_pending = True
+            self.set_value(value)
+            self.setVisible(False)
+            self.setVisible(True)       # Force widget layout update
+            self.read_pending = False
 
 
 class IntegerRessourceWidget(DataRessourceWidget):
@@ -72,7 +82,11 @@ class IntegerRessourceWidget(DataRessourceWidget):
         self.value_widget.valueChanged.connect(self.on_value_changed)
 
     def on_value_changed(self, value):
-        self.write_callback(self.key, value)
+        if self.read_pending == False:
+            self.write_callback(self.key, value)
+
+    def set_value(self, value):
+        self.value_widget.setValue(value)
 
 
 class FloatRessourceWidget(DataRessourceWidget):
@@ -86,7 +100,11 @@ class FloatRessourceWidget(DataRessourceWidget):
         self.value_widget.floatValueChanged.connect(self.on_value_changed)
 
     def on_value_changed(self, value):
-        self.write_callback(self.key, value)
+        if self.read_pending == False:
+            self.write_callback(self.key, value)
+
+    def set_value(self, value):
+        self.value_widget.setValue(value)
 
 
 class BoolRessourceWidget(DataRessourceWidget):
@@ -97,7 +115,11 @@ class BoolRessourceWidget(DataRessourceWidget):
         self.value_widget.stateChanged.connect(self.on_value_changed)
 
     def on_value_changed(self, value):
-        self.write_callback(self.key, value)
+        if self.read_pending == False:
+            self.write_callback(self.key, bool(value))
+
+    def set_value(self, value):
+        self.value_widget.setChecked(value)
 
 
 class EnumRessourceWidget(DataRessourceWidget):
@@ -110,7 +132,11 @@ class EnumRessourceWidget(DataRessourceWidget):
         self.value_widget.currentIndexChanged.connect(self.on_value_changed)
 
     def on_value_changed(self, index):
-        self.write_callback(self.key, self.value_widget.currentText())
+        if self.read_pending == False:
+            self.write_callback(self.key, self.value_widget.currentText())
+
+    def set_value(self, value):
+        self.value_widget.setCurrentIndex(self.value_widget.findText(value))
 
 
 class MainWindow(QMainWindow):
@@ -120,6 +146,8 @@ class MainWindow(QMainWindow):
         self.data_model = data_model
         self.widgets = {}
         self.setup_ui()
+        self.udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sequence = 0
 
     def load_model(self, data_model, container_layout, current_key = ''):
         for key, content in data_model.items():
@@ -175,13 +203,40 @@ class MainWindow(QMainWindow):
 
     def on_write_request(self, key, value):
         print(f'write {key} {value}')
-    
+        self.sequence = (self.sequence + 1) % 256
+        command = {'command' : 'write', 'sequence' : self.sequence, 'direction' : 'request', 'ressources' : {key : value}}
+        self.udp_client.sendto(json.dumps(command).encode('utf-8'), ("127.0.0.1", 5000))
+        response = json.loads(self.udp_client.recv(1024).decode("utf-8"))
+        if (response['command'] == "write" and response['sequence'] == self.sequence and response['direction'] == 'response'):
+            if response['status'][key] != 'success':
+                print(f'Error when writing ressource {key} {value}')
+
     def on_read_request(self, key):
         print(f'read {key}')
+        self.sequence = (self.sequence + 1) % 256
+        command = {'command' : 'read', 'sequence' : self.sequence, 'direction' : 'request', 'ressources' : [key]}
+        self.udp_client.sendto(json.dumps(command).encode('utf-8'), ("127.0.0.1", 5000))
+        response = json.loads(self.udp_client.recv(1024).decode("utf-8"))
+        if (response['command'] == "read" and response['sequence'] == self.sequence and response['direction'] == 'response'):
+            if response['status'][key] == 'success':
+                return True, response['ressources'][key]
+        print(f'Error when writing ressource {key}')
+        return False, None
 
-with open('data_model.json') as json_file:
-        data_model = json.load(json_file)
+if __name__ == '__main__':
 
-app = QApplication(sys.argv)
-w = MainWindow(data_model)
-sys.exit(app.exec_())
+    def on_service_state_change(zeroconf, service_type, name, state_change):
+        if state_change is ServiceStateChange.Added:
+            zeroconf.get_service_info(service_type, name)
+
+    zeroconf = Zeroconf()
+    ServiceBrowser(zeroconf, "_quadcopter._udp.local.", handlers=[on_service_state_change])
+
+    time.sleep(1)
+
+    with open('../data_model/data_model.json') as json_file:
+            data_model = json.load(json_file)
+
+    app = QApplication(sys.argv)
+    w = MainWindow(data_model)
+    sys.exit(app.exec_())

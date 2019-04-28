9b0c2b84867d6c36446dfd213f9da1fab7ad0a83
arnaudhe <arnaud.henric@gmail.com>
2019-04-28 23:20:47 +0200

    [soft] Reface API udp server

--- soft/main/hal/udp_server.cpp
+++ soft/main/hal/udp_server.cpp
@@ -8,6 +8,8 @@
 #include <lwip/sys.h>
 #include <lwip/netdb.h>
 
+#include <string.h>
+
 #include <esp_log.h>
 
 UdpServer::UdpServer(string name, int port)
@@ -15,77 +17,129 @@ UdpServer::UdpServer(string name, int port)
     _name = name;
     _port = port;
 
-    xTaskCreate(UdpServer::udp_server_task, "udp_server", 4096, this, 5, NULL);
+    struct sockaddr_in destAddr;
+    destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    destAddr.sin_family      = AF_INET;
+    destAddr.sin_port        = htons(_port);
+
+    _socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (_socket < 0) 
+    {
+        ESP_LOGE(_name.c_str(), "Unable to create socket: errno %d", errno);
+        return;
+    }
+
+    ESP_LOGI(_name.c_str(), "Socket created");
+
+    int err = bind(_socket, (struct sockaddr *)&destAddr, sizeof(destAddr));
+    if (err < 0) 
+    {
+        ESP_LOGE(_name.c_str(), "Socket unable to bind: errno %d", errno);
+    }
+
+    ESP_LOGI(_name.c_str(), "Server ready");
+}
+
+UdpServer::~UdpServer()
+{
+    close(_socket);
+}
+
+void UdpServer::start(void)
+{
+    xTaskCreate(UdpServer::udp_server_task, "udp_server", 8192, this, 5, NULL);
+}
+
+void UdpServer::register_callback(function<void(string, string&)> callback)
+{
+    _callback = callback;
+}
+
+bool UdpServer::recv(string &msg)
+{
+    char buffer[1024];
+    int  len;
+
+    len = ::recv(_socket, buffer, sizeof(buffer) - 1, 0);
+    if (len > 0)
+    {
+        buffer[len] = '\0';
+        string str(buffer);
+        msg = str;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool UdpServer::recvfrom(string &msg, string &address, int &port)
+{
+    struct sockaddr_in source_addr;
+    socklen_t          source_addr_len = sizeof(source_addr);
+    char               buffer[1024];
+    char             * addr_char;
+    int                len;
+
+    len = ::recvfrom(_socket, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&source_addr, &source_addr_len);
+
+    if (len > 0)
+    {
+        buffer[len] = '\0';
+        string str(buffer);
+        msg = str;
+        addr_char = inet_ntoa(source_addr.sin_addr);
+        string addr_str(addr_char);
+        address = addr_str;
+        port = ntohs(source_addr.sin_port);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool UdpServer::sendto(string msg, string address, int port)
+{
+    struct sockaddr_in dest_addr;
+    int                len;
+
+    inet_aton(address.c_str(), &dest_addr.sin_addr);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port   = htons(port);
+
+    len = ::sendto(_socket, msg.c_str(), strlen(msg.c_str()), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+
+    if (len > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 void UdpServer::udp_server_task(void *pvParameters)
 {
     UdpServer * server = (UdpServer *)pvParameters;
-    char rx_buffer[128];
-    char addr_str[128];
-    int addr_family;
-    int ip_protocol;
+    string      request;
+    string      response;
+    string      ip_src;
+    int         port_src;
 
     while (1) 
     {
-        struct sockaddr_in destAddr;
-        destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        destAddr.sin_family = AF_INET;
-        destAddr.sin_port = htons(server->_port);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
-        inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
+        ESP_LOGD(server->_name.c_str(), "Waiting for data");
 
-        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
-        if (sock < 0) {
-            ESP_LOGE(server->_name.c_str(), "Unable to create socket: errno %d", errno);
-            break;
-        }
-        ESP_LOGI(server->_name.c_str(), "Socket created");
-
-        int err = bind(sock, (struct sockaddr *)&destAddr, sizeof(destAddr));
-        if (err < 0) {
-            ESP_LOGE(server->_name.c_str(), "Socket unable to bind: errno %d", errno);
-        }
-        ESP_LOGI(server->_name.c_str(), "Socket binded");
-
-        while (1) 
+        if (server->recvfrom(request, ip_src, port_src))
         {
-            ESP_LOGI(server->_name.c_str(), "Waiting for data");
-            struct sockaddr_in6 sourceAddr; // Large enough for both IPv4 or IPv6
-            socklen_t socklen = sizeof(sourceAddr);
-            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&sourceAddr, &socklen);
-
-            // Error occured during receiving
-            if (len < 0) {
-                ESP_LOGE(server->_name.c_str(), "recvfrom failed: errno %d", errno);
-                break;
-            }
-            // Data received
-            else {
-                // Get the sender's ip address as string
-                if (sourceAddr.sin6_family == PF_INET) {
-                    inet_ntoa_r(((struct sockaddr_in *)&sourceAddr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
-                } else if (sourceAddr.sin6_family == PF_INET6) {
-                    inet6_ntoa_r(sourceAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
-                }
-
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
-                ESP_LOGI(server->_name.c_str(), "Received %d bytes from %s:", len, addr_str);
-                ESP_LOGI(server->_name.c_str(), "%s", rx_buffer);
-
-                int err = sendto(sock, rx_buffer, len, 0, (struct sockaddr *)&sourceAddr, sizeof(sourceAddr));
-                if (err < 0) {
-                    ESP_LOGE(server->_name.c_str(), "Error occured during sending: errno %d", errno);
-                    break;
-                }
-            }
-        }
-
-        if (sock != -1) {
-            ESP_LOGE(server->_name.c_str(), "Shutting down socket and restarting...");
-            shutdown(sock, 0);
-            close(sock);
+            ESP_LOGI(server->_name.c_str(), "Received from %s:%d : %s", ip_src.c_str(), port_src, request.c_str());
+            server->_callback(request, response);
+            ESP_LOGI(server->_name.c_str(), "Send to %s:%d : %s", ip_src.c_str(), port_src, response.c_str());
+            server->sendto(response, ip_src, port_src);
         }
     }
     vTaskDelete(NULL);
--- soft/main/hal/udp_server.h
+++ soft/main/hal/udp_server.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <functional>
 
 using namespace std;
 
@@ -8,12 +9,26 @@ class UdpServer
 {
   private:
 
-    string  _name;
-    int     _port;
+    string                              _name;
+    int                                 _port;
+    function<void(string, string&)>     _callback;
+    int                                 _socket;
 
     static void udp_server_task(void *);
+
+    bool recv(string &msg);
+
+    bool recvfrom(string &msg, string &address, int &port);
+
+    bool sendto(string msg, string address, int port);
 
   public:
 
     UdpServer(string name, int port);
+    ~UdpServer();
+
+    void register_callback(function<void(string, string&)>);
+
+    void start(void);
+
 };

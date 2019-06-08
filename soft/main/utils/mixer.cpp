7bdfcf65df37de205408d798ae070aaea8910c9f
arnaudhe <arnaud.henric@gmail.com>
2019-06-08 17:32:58 +0200

    [soft] Invert pitch control commands in mixer

--- soft/main/utils/mixer.cpp
+++ soft/main/utils/mixer.cpp
@@ -12,10 +12,10 @@ Mixer::Mixer(Motor * front_left, Motor * front_right, Motor * rear_left, Motor *
 
 void Mixer::update(float height_command, float roll_command, float pitch_command, float yaw_command)
 {
-    _front_left->set_speed ( height_command - roll_command - pitch_command - yaw_command);
-    _front_right->set_speed( height_command + roll_command - pitch_command + yaw_command);
-    _rear_right->set_speed ( height_command + roll_command + pitch_command - yaw_command);
-    _rear_left->set_speed  ( height_command - roll_command + pitch_command + yaw_command);
+    _front_left->set_speed( height_command - roll_command + pitch_command - yaw_command);
+    _front_right->set_speed( height_command + roll_command + pitch_command + yaw_command);
+    _rear_right->set_speed( height_command + roll_command - pitch_command - yaw_command);
+    _rear_left->set_speed( height_command - roll_command - pitch_command + yaw_command);
 }
 
 void Mixer::set_motors_speeds(float front_left, float front_right, float rear_left, float rear_right)

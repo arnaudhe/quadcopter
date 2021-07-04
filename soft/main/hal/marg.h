eef818fc6f713ab55b64195e9da9b7df93d9ad53
arnaudhe <arnaud.henric@gmail.com>
2021-07-05 00:33:52 +0200

    [sensors] Add multi-bytes I2C operations to reduce reading duration

--- soft/main/hal/marg.h
+++ soft/main/hal/marg.h
@@ -33,5 +33,8 @@ class Marg
 
     esp_err_t read_gyro(float * gyro_x, float * gyro_y, float * gyro_z);
 
+    esp_err_t read_acc_gyro(float *acc_x, float *acc_y, float *acc_z,
+                            float *gyro_x, float *gyro_y, float *gyro_z);
+
     esp_err_t read_mag(float * mag_x, float * mag_y, float * mag_z);
 };
--- soft/main/periph/i2c_master.h
+++ soft/main/periph/i2c_master.h
@@ -16,13 +16,6 @@ class I2cMaster
     i2c_port_t _port;       ///< i2c port
     bool       _init;
 
-
-    /* Methods */
-
-    esp_err_t _write(uint8_t address, uint8_t * data, uint8_t data_len);
-
-    esp_err_t _read(uint8_t address, uint8_t * data, uint8_t data_len);
-
   public:
 
     /* Constructors */
@@ -37,7 +30,9 @@ class I2cMaster
 
     esp_err_t deinit();
 
-    esp_err_t read(uint8_t address, uint8_t reg, uint8_t * value, uint8_t len);
+    esp_err_t read_registers(uint8_t address, uint8_t reg, uint8_t * value, uint8_t len);
+
+    esp_err_t write_registers(uint8_t address, uint8_t reg, uint8_t * value, uint8_t len);
 
     esp_err_t read_register(uint8_t address, uint8_t reg, uint8_t * value);
 
@@ -46,9 +41,5 @@ class I2cMaster
     esp_err_t write_bit(uint8_t address, uint8_t reg, uint8_t bit_num, bool value);
 
     esp_err_t read_bit(uint8_t address, uint8_t reg, uint8_t bit_num, bool * value);
-
-    esp_err_t read_int(uint8_t address, uint8_t reg, int16_t * value);
-
-    esp_err_t read_uint(uint8_t address, uint8_t reg, uint16_t * value);
 
 };

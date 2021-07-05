be64705906ad365aee25ed4353bddcaaeae02760
arnaudhe <arnaud.henric@gmail.com>
2021-07-05 15:00:34 +0200

    [PID] Add D-term lowpass filter

--- soft/main/utils/pid.cpp
+++ soft/main/utils/pid.cpp
@@ -1,26 +1,30 @@
 #include <utils/pid.h>
 #include <esp_attr.h>
 
+#define PID_LOWPASS_CUTOFF_FREQUENCY    40.0
+
 Pid::Pid(float period, float kp, float ki, float kd) 
 {
-    _consign   = 0;
-    _previous  = 0;
-    _integrate = 0;
-    _period    = period;
-    _kp        = kp;
-    _ki        = ki;
-    _kd        = kd;
+    _consign      = 0;
+    _previous     = 0;
+    _integrate    = 0;
+    _period       = period;
+    _kp           = kp;
+    _ki           = ki;
+    _kd           = kd;
+    _dterm_filter = new PT2Filter(period, PID_LOWPASS_CUTOFF_FREQUENCY);
 }
 
 float IRAM_ATTR Pid::update(float input)
 {
-    float error     = _consign - input;
-    float derivate  = (error - _previous) / (_period);
-    float integrate = _integrate + (error * _period);
+    float error           = _consign - input;
+    float derivate        = (error - _previous) / (_period);
+    float integrate       = _integrate + (error * _period);
+    float filter_derivate = _dterm_filter->apply(derivate);
 
     float output    = _kp * error +
                       _ki * integrate +
-                      _kd * derivate;
+                      _kd * filter_derivate;
 
     _integrate = integrate;
     _previous = error;

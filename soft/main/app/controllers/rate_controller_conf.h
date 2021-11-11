6c7c3d8fe7055ce9235070766dc6d63cd7fa3eeb
arnaudhe <arnaud.henric@gmail.com>
2021-11-11 16:40:11 +0100

    [rate ctrl] PID tuning

--- soft/main/app/controllers/rate_controller_conf.h
+++ soft/main/app/controllers/rate_controller_conf.h
@@ -3,14 +3,14 @@
 #define RATE_GYRO_FILTER_FREQ_SLOPE   (250.0f)
 #define RATE_GYRO_FILTER_FREQ_CUTOFF  (12.0f)
 
-#define RATE_PID_ROLL_KP        (0.08f)
-#define RATE_PID_ROLL_KI        (0.012f)
-#define RATE_PID_ROLL_KD        (0.0f)
+#define RATE_PID_ROLL_KP        (0.025f)
+#define RATE_PID_ROLL_KI        (0.002f)
+#define RATE_PID_ROLL_KD        (0.003f)
 #define RATE_PID_ROLL_FF        (0.015f)
 
-#define RATE_PID_PITCH_KP       (0.08f)
-#define RATE_PID_PITCH_KI       (0.012f)
-#define RATE_PID_PITCH_KD       (0.0f)
+#define RATE_PID_PITCH_KP       (0.025f)
+#define RATE_PID_PITCH_KI       (0.002f)
+#define RATE_PID_PITCH_KD       (0.003f)
 #define RATE_PID_PITCH_FF       (0.015f)
 
 #define RATE_PID_YAW_KP         (0.5f)

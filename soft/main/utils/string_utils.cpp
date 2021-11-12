b701e391a3bedb15973aeb204351d14a8084523d
Arnaud Henric <arnaud.henric@gmail.com>
2021-11-12 17:06:42 +0100

    [telemetry] Add telemetry module

---
+++ soft/main/utils/string_utils.cpp
@@ -0,0 +1,18 @@
+#include <utils/string_utils.h>
+
+vector<string> StringUtils::split(string s, string delimiter)
+{
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find (delimiter, pos_start)) != string::npos)
+    {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
---
+++ soft/main/utils/string_utils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class StringUtils
+{
+
+public:
+
+    static vector<string> split(string s, string delimiter);
+
+};

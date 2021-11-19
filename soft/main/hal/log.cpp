adb0a12bcfb9edd4bd9074631c5f6bbd7d432cfb
arnaudhe <arnaud.henric@gmail.com>
2021-11-20 00:46:54 +0100

    [log] Add log level threshold define and verbose log domain

--- soft/main/hal/log.cpp
+++ soft/main/hal/log.cpp
@@ -16,14 +16,18 @@ void IRAM_ATTR Logger::Verbose(const char * filename, const char * fmt, ...)
     char buffer[256];
     va_list args;
 
-    va_start(args, fmt);
-    vsprintf(buffer, fmt, args);
-    va_end(args);
-    esp_log_write(ESP_LOG_VERBOSE, filename, LOG_COLOR_V "V (%d) %s: %s\n" LOG_RESET_COLOR, esp_log_timestamp(), filename, buffer);
-
-    if (_logger)
+    if ((strcmp(filename, LOG_VERBOSE_FILE) == 0) ||
+        (strcmp("*", LOG_VERBOSE_FILE) == 0))
     {
-        _logger->_udp->send_broadcast(buffer, LOGGER_DEST_PORT);
+        va_start(args, fmt);
+        vsprintf(buffer, fmt, args);
+        va_end(args);
+        esp_log_write(ESP_LOG_VERBOSE, filename, LOG_COLOR_V "V (%d) %s: %s\n" LOG_RESET_COLOR, esp_log_timestamp(), filename, buffer);
+
+        if (_logger)
+        {
+            _logger->_udp->send_broadcast(buffer, LOGGER_DEST_PORT);
+        }
     }
 }
 
--- soft/main/hal/log.h
+++ soft/main/hal/log.h
@@ -6,11 +6,46 @@
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
+#define LOG_LEVEL_VERBOSE       0       // Module's developer useful data. Can be very verbose. (buffer dumps, bus data dump, ...)
+#define LOG_LEVEL_DEBUG         1       // Debugging data that can help understanding the execution (state machine transitions, variable value, ...)
+#define LOG_LEVEL_INFO          2       // A general event (not periodic) about runtime (connection done, system disarmed, alarm triggered, ...)
+#define LOG_LEVEL_WARNING       3       // An external (not caused by runtime) defective event that MAY affect the runtime (frame lost, data corrupted, ...)
+#define LOG_LEVEL_ERROR         4       // A defect that WILL affect the runtime (communication failure, bad state, ...)
+#define LOG_LEVEL_DISABLE       0xff    // Use to disable the logs
+
+#define LOG_LEVEL               LOG_LEVEL_DEBUG
+
+#define LOG_VERBOSE_FILE        "file.cpp"
+
+#if (LOG_LEVEL <= LOG_LEVEL_VERBOSE)
 #define LOG_VERBOSE(fmt, ...)   Logger::Verbose(__FILENAME__, fmt, ##__VA_ARGS__)
+#else
+#define LOG_VERBOSE(fmt, ...)
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_DEBUG)
 #define LOG_DEBUG(fmt, ...)     Logger::Debug(__FILENAME__, fmt, ##__VA_ARGS__)
+#else
+#define LOG_DEBUG(fmt, ...)
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_INFO)
 #define LOG_INFO(fmt, ...)      Logger::Info(__FILENAME__, fmt, ##__VA_ARGS__)
+#else
+#define LOG_INFO(fmt, ...)
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_WARNING)
 #define LOG_WARNING(fmt, ...)   Logger::Warning(__FILENAME__, fmt, ##__VA_ARGS__)
+#else
+#define LOG_WARNING(fmt, ...)
+#endif
+
+#if (LOG_LEVEL <= LOG_LEVEL_ERROR)
 #define LOG_ERROR(fmt, ...)     Logger::Error(__FILENAME__, fmt, ##__VA_ARGS__)
+#else
+#define LOG_ERROR(fmt, ...)
+#endif
 
 class Logger
 {

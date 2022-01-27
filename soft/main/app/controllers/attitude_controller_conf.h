57b835236114b15a2a3e69ea2b2cb3c09fc43a34
arnaudhe <arnaud.henric@gmail.com>
2022-01-27 19:21:57 +0100

    [attitude ctrl] Update tuning

--- soft/main/app/controllers/attitude_controller_conf.h
+++ soft/main/app/controllers/attitude_controller_conf.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#define ATTITUDE_PID_ROLL_POSITION_KP               (0.8f)
-#define ATTITUDE_PID_ROLL_POSITION_KI               (0.1f)
+#define ATTITUDE_PID_ROLL_POSITION_KP               (1.5f)
+#define ATTITUDE_PID_ROLL_POSITION_KI               (0.2f)
 #define ATTITUDE_PID_ROLL_POSITION_KD               (0.0f)
 #define ATTITUDE_PID_ROLL_POSITION_FF               (0.0f)
-#define ATTITUDE_PID_ROLL_POSITION_AW               (0.0f)
+#define ATTITUDE_PID_ROLL_POSITION_AW               (6.0f)
 
-#define ATTITUDE_PID_PITCH_POSITION_KP              (0.8f)
-#define ATTITUDE_PID_PITCH_POSITION_KI              (0.1f)
+#define ATTITUDE_PID_PITCH_POSITION_KP              (1.5f)
+#define ATTITUDE_PID_PITCH_POSITION_KI              (0.2f)
 #define ATTITUDE_PID_PITCH_POSITION_KD              (0.0f)
 #define ATTITUDE_PID_PITCH_POSITION_FF              (0.0f)
-#define ATTITUDE_PID_PITCH_POSITION_AW              (0.0f)
+#define ATTITUDE_PID_PITCH_POSITION_AW              (6.0f)
 
 #define ATTITUDE_PID_YAW_POSITION_KP                (0.12f)
 #define ATTITUDE_PID_YAW_POSITION_KI                (0.12f)
@@ -18,5 +18,6 @@
 #define ATTITUDE_PID_YAW_POSITION_FF                (0.00f)
 #define ATTITUDE_PID_YAW_POSITION_AW                (0.01f)
 
-#define ATTITUDE_MAX_RATE_COMMAND                   (5.0f)
+#define ATTITUDE_MAX_RATE_COMMAND                   (2.0f)
+
 #define ATTITUDE_MAX_SLEW_COMMAND                   (4.0f)

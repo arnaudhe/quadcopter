5d7500587860096d3be9ab553640de7633f87bb7
arnaudhe <arnaud.henric@gmail.com>
2022-03-26 17:55:45 +0100

    [height observer] Tuning & restoring zero velocity detector to improve stability

--- soft/main/app/observers/height_observer.cpp
+++ soft/main/app/observers/height_observer.cpp
@@ -6,7 +6,7 @@
 #define ULTRASOUND_MAX_RANGE_m              3.0f
 
 HeightObserver::HeightObserver(float period):
-    Kalman(period, 2, 1, 1)
+    Kalman(period, 2, 1, 2)
 {
     _state_matrix.set(0, 0, 1.0);
     _state_matrix.set(0, 1, period);
@@ -18,6 +18,8 @@ HeightObserver::HeightObserver(float period):
 
     _output_matrix.set(0, 0, 1.0);
     _output_matrix.set(0, 1, 0.0);
+    _output_matrix.set(1, 0, 0.0);
+    _output_matrix.set(1, 1, 1.0);
 
     _process_noise_matrix.set(0, 0, powf(period, 4) / 4 * HEIGHT_OBSERVER_ACCELEROMETER_DEVIATION * HEIGHT_OBSERVER_ACCELEROMETER_DEVIATION);
     _process_noise_matrix.set(0, 1, powf(period, 3) / 2 * HEIGHT_OBSERVER_ACCELEROMETER_DEVIATION * HEIGHT_OBSERVER_ACCELEROMETER_DEVIATION);
@@ -25,6 +27,7 @@ HeightObserver::HeightObserver(float period):
     _process_noise_matrix.set(1, 1, powf(period, 2) * HEIGHT_OBSERVER_ACCELEROMETER_DEVIATION * HEIGHT_OBSERVER_ACCELEROMETER_DEVIATION);
 
     _measure_noise_matrix.set(0, 0, HEIGHT_OBSERVER_SENSOR_BAROMETER_DEVIATION * HEIGHT_OBSERVER_SENSOR_BAROMETER_DEVIATION);
+    _measure_noise_matrix.set(1, 1, HEIGHT_OBSERVER_ZERO_VELOCITY_DEVIATION * HEIGHT_OBSERVER_ZERO_VELOCITY_DEVIATION);
 
     _covariance.set(0, 0, 0.01);
     _covariance.set(1, 1, 0.01);
@@ -32,8 +35,20 @@ HeightObserver::HeightObserver(float period):
 
 void HeightObserver::update(float acc_z, float baro, float ultrasound)
 {
-    Matrix input(1, 1, acc_z);
-    Matrix measure(1, 1, baro);
+    Matrix input(1, 1, acc_z - 0.15f);
+    Matrix measure(2, 1, 0.0f);
+
+    measure.set(0, 0, baro);
+    measure.set(1, 0, 0.0);
+
+    if (fabs(acc_z - 0.15f) < 0.2f)
+    {
+        _output_matrix.set(1, 1, 1.0);
+    }
+    else
+    {
+        _output_matrix.set(1, 1, 0.0);
+    }
 
     Kalman::update(input, measure);
 }

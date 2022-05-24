63a242f21b74c7143094d5f39d1d42c9b37fd222
arnaudhe <arnaud.henric@gmail.com>
2022-05-25 00:48:36 +0200

    [radio command] Fix I2C port

--- software/radio_command/platform.hpp
+++ software/radio_command/platform.hpp
@@ -10,11 +10,10 @@
 #define PLATFORM_ROLL_STICK_ADC_CHANNEL         2
 #define PLATFORM_PITCH_STICK_ADC_CHANNEL        3
 
-#define PLATFORM_BATTERY_I2C             "dev/i2c-1"
-#define PLATFORM_BATTERY_I2C_ADDRESS     0x49
-#define PLATFORM_BATTERY_CHANNEL         0
-
 #define PLATFORM_SI4432_SPI             "/dev/spidev0.0"
 #define PLATFORM_SI4432_SPI_SPEED       1000000
 #define PLATFORM_SI4432_IRQ             22
+#define PLATFORM_BATTERY_I2C                    "/dev/i2c-1"
+#define PLATFORM_BATTERY_I2C_ADDRESS            0x49
+#define PLATFORM_BATTERY_CHANNEL                0
 

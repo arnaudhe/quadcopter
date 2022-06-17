d13838b69fa1c98b0169c205baf5015d5a5d1c1f
arnaudhe <arnaud.henric@gmail.com>
2022-06-17 12:38:15 +0200

    [rc] Add framebuffer abstraction class to display image on screen

---
+++ software/radio_command/hal/framebuffer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdint.h>
+#include <linux/fb.h>
+#include <utils/pixmap.hpp>
+class FrameBuffer
+{
+
+private:
+
+    int       _fd;
+    uint8_t * _fbp;
+    int       _xoffset;
+    int       _yoffset;
+    int       _width;
+    int       _height;
+    int       _bytes_per_pixel;
+    int       _line_length;
+
+public:
+
+    FrameBuffer(const char * device);
+    ~FrameBuffer(void);
+
+    void update(RGB565PixMap * image);
+
+    int width();
+
+    int height();
+
+    int size();
+
+};
